d3108fc8d9f01b43d9680e843881e39ca6ec6686
Leila <nb.leila10@gmail.com>
2019-03-01 13:29:58 -0600

    Implement the POST request
    Response to a post request
    Endpoint: /api/posts
    Return the newly created post, if all information is correct. Otherwise sends an error message

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -46,4 +46,24 @@ router.get("/:id", async (req, res) => {
   }
 })
 
+router.post("/", async (req, res) => {
+  const post = req.body
+  if (!post.title || !post.contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post." })
+  } else {
+    try {
+      await db.insert(post)
+      res.status(201).json(post)
+    } catch (error) {
+      // log error to database
+      console.log(error)
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      })
+    }
+  }
+})
+
 module.exports = router
