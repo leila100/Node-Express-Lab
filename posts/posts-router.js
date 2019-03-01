e94a86847ec32b7b45a5e001100dc9ca8cb5948f
Leila <nb.leila10@gmail.com>
2019-03-01 13:18:49 -0600

    Implement the GET route
    Response to a get request for a specific id
    Endpoint: /api/posts/:id
    Returns the specific post, if found. Otherwise sends an error message

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -16,9 +16,33 @@ router.get("/", async (req, res) => {
   } catch (error) {
     // log error to database
     console.log(error)
-    res.status(500).json({
-      message: "Error retrieving the posts"
-    })
+    res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved." })
+  }
+})
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.status(400).json({ error: "The id has to be a number" })
+  } else {
+    try {
+      const post = await db.findById(id)
+      if (post.length !== 0) {
+        res.status(200).json(post)
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." })
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error)
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." })
+    }
   }
 })
 
